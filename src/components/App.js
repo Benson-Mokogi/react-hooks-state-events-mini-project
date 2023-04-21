02bef954e5c381cabdaf76ad7a24a51a972a2e97
Benson Mokogi <b.mokogi@gmail.com>
2023-04-22 01:35:50 +0300

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [category, setCategory] = useState("All");
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const filteredTasks = category === "All" ? tasks : tasks.filter((task) => task.category === category);
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} selectedCategory={category} onSelectCategory={setCategory} />
+      <NewTaskForm onAddTask={(newTask) => setTasks([...tasks, newTask])} />
+      <TaskList tasks={filteredTasks} onDeleteTask={deleteTask} />
     </div>
   );
 }
